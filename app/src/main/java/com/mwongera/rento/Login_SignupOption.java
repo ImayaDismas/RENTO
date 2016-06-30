244643f3c6cefce1092e520298c65fb79b96b6ed
ImayaDismas <imayadismas@gmail.com>
2016-06-30 17:21:15 +0300

    Created a empty layouts and activities

---
+++ app/src/main/java/com/mwongera/rento/Login_SignupOption.java
@@ -0,0 +1,16 @@
+package com.mwongera.rento;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by imaya on 6/30/16.
+ */
+public class Login_SignupOption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.option_login_signup);
+    }
+}
---
+++ app/src/main/java/com/mwongera/rento/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.mwongera.rento;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by imaya on 6/30/16.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
+
