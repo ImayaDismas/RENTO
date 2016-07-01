b6cf4b10efd3844aa150f86a8ddc849da33556a0
mwongeraE <evansmwongera@gmail.com>
2016-07-01 07:36:49 +0300

    color tabs

--- app/src/main/java/com/mwongera/rento/MainActivity.java
+++ app/src/main/java/com/mwongera/rento/MainActivity.java
@@ -17,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private int[] tabIcons = {
+            R.drawable.home,
+            R.drawable.car,
+            R.drawable.listed,
+            R.drawable.profile
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +39,22 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+    }
+
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+        tabLayout.getTabAt(3).setIcon(tabIcons[3]);
     }
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new OneFragment(), "ONE");
-        adapter.addFragment(new TwoFragment(), "TWO");
-        adapter.addFragment(new ThreeFragment(), "THREE");
-        adapter.addFragment(new FourFragment(), "FOUR");
+        adapter.addFragment(new OneFragment(), "HOME");
+        adapter.addFragment(new TwoFragment(), "RENT");
+        adapter.addFragment(new ThreeFragment(), "LIST");
+        adapter.addFragment(new FourFragment(), "PROFILE");
         viewPager.setAdapter(adapter);
     }
 
