db20751ed80068bb8b403f1361d75116e9a75eee
ImayaDismas <imayadismas@gmail.com>
2016-07-09 02:51:14 +0300

    Connnected all the activities and layouts

--- app/src/main/java/com/mwongera/rento/Login_SignupOption.java
+++ app/src/main/java/com/mwongera/rento/Login_SignupOption.java
@@ -1,16 +1,61 @@
 package com.mwongera.rento;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by imaya on 6/30/16.
  */
 public class Login_SignupOption extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.option_login_signup);
+        addListenerOnButton();
+        addListenerOnButton1();
+    }
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, SignupActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+    public void addListenerOnButton1() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, LoginActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
