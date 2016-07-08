8c14f959ef252ac41c15caa2b1fb04c4e6c23b13
ImayaDismas <imayadismas@gmail.com>
2016-07-08 23:47:16 +0300

    Added the splash activity

--- app/src/main/java/com/mwongera/rento/SplashActivity.java
+++ app/src/main/java/com/mwongera/rento/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.mwongera.rento;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -12,6 +13,27 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
     }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
 }
 
