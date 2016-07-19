372b2404f90f453685e9e4ada424a29f7c55a822
mwongeraE <evansmwongera@gmail.com>
2016-07-19 05:13:19 +0300

    identity form

--- app/src/main/java/com/mwongera/rento/Identityrecyclerview.java
+++ app/src/main/java/com/mwongera/rento/Identityrecyclerview.java
@@ -1,7 +1,23 @@
 package com.mwongera.rento;
 
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 /**
  * Created by mwongera on 19/07/16.
  */
 public class Identityrecyclerview {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.identity_recycler);
+        RecyclerView recList = (RecyclerView) findViewById(R.id.cardlist);
+        recList.setHasFixedSize(false);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recList.setLayoutManager(llm);
+    }
+
 }
